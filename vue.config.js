c8d85a4ab3124fbe8b4c380a85b4a5db64f2467e
coderzhong <571314943@qq.com>
2019-05-07 11:44:54 +0800

    添加vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,8 @@
+const isProduction = process.env.NODE_ENV === 'production'
+module.exports = {
+  publicPath: isProduction ? './' : '/',
+  productionSourceMap: false,
+  devServer: {
+    proxy: {}
+  }
+}
