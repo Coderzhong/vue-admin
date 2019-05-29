f6a64bc9cc8610772070a3be19867716aac0c042
zhongxiaoxia <xiaoxia.zhong@ecarx.com.cn>
2019-05-29 19:14:13 +0800

    添加eslint的Standard Style链接地址

--- src/views/document/Conventions.vue
+++ src/views/document/Conventions.vue
@@ -1,5 +1,11 @@
 <template>
   <div>
+    <h1>代码规范</h1>
+    <ul>
+      <li>
+        <a target="_blank" href="https://github.com/standard/standard/blob/master/docs/RULES-zhcn.md">eslint的Standard Style</a>
+      </li>      
+    </ul>
     <h1>命名规范</h1>
     <ul>
       <li>
