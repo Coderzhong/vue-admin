15047ea8d367c7306f57d963381173e046ac1de5
zhongxiaoxia <xiaoxia.zhong@ecarx.com.cn>
2019-07-01 18:08:04 +0800

    添加说明

--- src/lib/request.js
+++ src/lib/request.js
@@ -23,6 +23,9 @@ request.interceptors.request.use(config => {
 
 request.interceptors.response.use(res => {
   const { code, message, data } = res.data
+  // 1.如果后端做了登录超时的控制，返回的数据中code为401
+  // 那么需要在此处做一个拦截，进行提示，并触发store的logout
+  // 2.根据其他的状态码比如403 500等的路由跳转到对应的错误页
   if (code && code !== 0) {
     MessageBox.alert(message, {
       type: 'warning'
